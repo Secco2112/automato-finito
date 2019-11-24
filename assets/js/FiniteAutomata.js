9773f76b1f50f22060113830e7575da8d884e97d
Secco2112 <gustavomarini9@gmail.com>
2019-11-24 17:12:27 -0300

    Finalizando

--- assets/js/FiniteAutomata.js
+++ assets/js/FiniteAutomata.js
@@ -1,7 +1,20 @@
 class FiniteAutomata{
 
     constructor() {
+        this.global_state = 0;
         this.tokens = [];
+        this.states = [[]];
+        this.draw_table = [];
+        this.table_element = $("table");
+    }
+
+    setGlobalState(global_state) {
+        this.global_state = global_state;
+        return this;
+    }
+
+    getGlobalState() {
+        return this.global_state;
     }
 
     addToken(token) {
@@ -30,6 +43,79 @@ class FiniteAutomata{
     clearTokens() {
         this.tokens = [];
         return this;
+    }
+
+    setStates(states) {
+        this.states = states;
+        return this;
+    }
+
+    getStates() {
+        return this.states;
+    }
+
+    setDrawTable(draw_table) {
+        this.draw_table = draw_table;
+        return this;
+    }
+
+    getDrawTable() {
+        return this.draw_table;
+    }
+
+    setTableElement(table_element) {
+        this.table_element = table_element;
+        return this;
+    }
+
+    getTableElement() {
+        return this.table_element;
+    }
+
+    validWord(word) {
+        var state = 0,
+            error = false;
+
+        this.table_element.find("td").each(function(i, td) {
+            $(td).removeClass("focus-item focus-column focus-row focus-row-error focus-column-error");
+        });
+
+        for(var i=0; i<word.length; i++) {
+            if(!error) {
+                if(this.draw_table[state][word[i]] != '-') {
+                    state = this.draw_table[state][word[i]];
+                } else {
+                    error = true;
+                }
+            }
+        }
+
+        if(!error) {
+            var row = this.table_element.find("tr#row[data-row='" + (state - 1) + "']");
+            $(row).find("td").each(function() {
+                $(this).addClass("focus-row");
+            });
+            var item = $(row).find("td#column[data-column='" + word[state - 1] + "']");
+            this.table_element.find("tbody tr td").each(function() {
+                if($(this).index() == item.index()) {
+                    $(this).addClass("focus-column");
+                }
+            });
+            $(item).removeClass("focus-row focus-column").addClass("focus-item");
+        } else {
+            var row = this.table_element.find("tr#row[data-row='" + (state - 1) + "']");
+            $(row).find("td").each(function() {
+                $(this).addClass("focus-row-error");
+            });
+            var item = $(row).find("td#column[data-column='" + word[state - 1] + "']");
+            this.table_element.find("tbody tr td").each(function() {
+                if($(this).index() == item.index()) {
+                    $(this).addClass("focus-column-error");
+                }
+            });
+        }
+
+        return !error;
     }
 
 }
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 const finite_automata = new FiniteAutomata();
+finite_automata.setTableElement($(".table-wrapper table"));
 
 
 $(document).ready(function() {
@@ -20,6 +21,11 @@ function addToken() {
 
                         var html = "<li><span>" + v.trim() + "</span><i class='fa fa-times delete-token'></i></li>";
                         $(".list-words .list").append(html);
+                        
+                        generateStates();
+                        generateDrawTable();
+                        drawTable();
+                        rebuildTokenInput();
                     }
                 }
             });
@@ -37,9 +43,36 @@ function addToken() {
 
 
 function handleTestTokenInput() {
-    $("#words-form .word-listener").on("input", function(e) {
-        var value = $(this).html();
+    $(".word-listener").on("focusin", function() {
+        $("input[name=fake-token-input]").focus();
     });
+
+    $("input[name=fake-token-input]").on("keyup", function(e) {
+        var text = $(this).val();
+
+        $(".word-listener").empty();
+
+        var values = text.trim().split(" ");
+        var html = "";
+        $.each(values, function(i, word) {
+            html += "<span class='" + (finite_automata.validWord(word)? "valid": "invalid") + "'>" + word + "</span> ";
+        });
+        $(".word-listener").html(html);
+    });
+}
+
+
+function rebuildTokenInput() {
+    var text = $("input[name=fake-token-input]").val();
+
+    $(".word-listener").empty();
+
+    var values = text.trim().split(" ");
+    var html = "";
+    $.each(values, function(i, word) {
+        html += "<span class='" + (finite_automata.validWord(word)? "valid": "invalid") + "'>" + word + "</span> ";
+    });
+    $(".word-listener").html(html);
 }
 
 
@@ -48,5 +81,95 @@ function deleteToken() {
         var token = $(this).parent().find("span").text();
         finite_automata.removeToken(token);
         $(this).parent().remove();
+
+        finite_automata.setStates([[]]);
+        finite_automata.setDrawTable([]);
+        finite_automata.setGlobalState(0);
+        
+        generateStates();
+        generateDrawTable();
+        drawTable();
+        rebuildTokenInput();
+    });
+}
+
+
+function generateStates() {
+    var words = finite_automata.getTokens(),
+        states = finite_automata.getStates();
+    
+    $.each(words, function(index, word) {
+        var current_state = 0;
+        for(var i=0; i<word.length; i++) {
+            if(typeof states[current_state][word[i]] == 'undefined') {
+                var next_state = finite_automata.getGlobalState() + 1;
+                states[current_state][word[i]] = next_state;
+                states[next_state] = [];
+                finite_automata.setGlobalState(next_state);
+                current_state = next_state;
+            } else {
+                current_state = states[current_state][word[i]];
+            }
+
+            if(i == word.length - 1) states[current_state]['final'] = 1;
+        }
+    });
+
+    finite_automata.setStates(states);
+}
+
+
+function generateDrawTable() {
+    var draw_table = [],
+        states = finite_automata.getStates();
+
+    $.each(states, function(i, state) {
+        var tmp = {
+            'estado': i
+        };
+        
+        for(var j=97; j<123; j++) {
+            var letter = String.fromCharCode(j);
+
+            if(typeof state[letter] == 'undefined') {
+                tmp[letter] = '-';
+            } else {
+                tmp[letter] = state[letter];
+            }
+        }
+
+        if(state['final'] == 1) tmp['final'] = 1;
+
+        draw_table.push(tmp);
+    });
+
+    finite_automata.setDrawTable(draw_table);
+}
+
+
+function drawTable() {
+    var table = finite_automata.getDrawTable();
+    $(".table-wrapper table tbody").empty();
+
+    $.each(table, function(i, item) {
+        var html = "<tr id='row' data-row='" + item['estado'] + "'>";
+                if(item['final']) {
+                    html += "<td>q" + item['estado'] + "*</td>";
+                } else {
+                    html += "<td>q" + item['estado'] + "</td>";
+                }
+
+                for(var j=97; j<123; j++) {
+                    var letter = String.fromCharCode(j);
+                
+                    if(item[letter] != '-') {
+                        html += "<td id='column' data-column='" + letter + "'>q" + item[letter] + "</td>";
+                    } else {
+                        html += "<td>-</td>";
+                    }
+                }
+            html += "</tr>";
+
+        $(".table-wrapper table tbody").append(html);
     });
 }
